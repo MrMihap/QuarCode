db957d501114be52a02088b42f2ea1bf23dd986a
Mikhail <mrmihap@yandex.ru>
2015-05-09 00:11:14 +0300

    init commit
    some base classes

---
+++ Quarcode/Core/Types.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quarcode.Core
+{
+  public struct Vector
+  {
+    double x;
+    double y;
+    void Init()
+    {
+      x = 0;
+      y = 0;
+    }
+    void Init(double _x, double _y)
+    {
+    }
+  }
+}
