4c2f6861a043f83644c02e8a04d2f00da0651a90
MrMihap <MrMihap@yandex.ru>
2015-07-22 16:52:07 +0300

    new projects

---
+++ recognDebug/FMainForm.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace recognDebug
+{
+  partial class FMainForm
+  {
+    /// <summary>
+    /// Требуется переменная конструктора.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Освободить все используемые ресурсы.
+    /// </summary>
+    /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Код, автоматически созданный конструктором форм Windows
+
+    /// <summary>
+    /// Обязательный метод для поддержки конструктора - не изменяйте
+    /// содержимое данного метода при помощи редактора кода.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.SuspendLayout();
+      // 
+      // FMainForm
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(1531, 687);
+      this.Name = "FMainForm";
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+  }
+}
+
