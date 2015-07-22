3d00af96e812933b8c12ac4f1a7f6755fdc236ad
MrMihap <MrMihap@yandex.ru>
2015-07-22 17:32:42 +0300

    Base Arch

---
+++ recognTools/IDataRecievers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace recognTools
+{
+
+  public interface IRecieveRecognizedCode
+  {
+    void Recieve (string code);
+  }
+  public interface IRecieveRawImage
+  {
+    void Recieve(Image<Bgr, Byte> sourse);
+  }
+  public interface IRecieveFilteredImage
+  {
+    void Recieve(Image<Bgr, Byte> sourse);
+  }
+  public interface IRecieveFoundContours
+  {
+    void Recieve(List<MCvContour> contourList);
+  }
+  public interface IRecieveCroptedImage
+  {
+    void Recieve(Image<Bgr, Byte> sourse);
+  }
+}
--- recognTools/ImageParser.cs
+++ recognTools/ImageParser.cs
@@ -3,10 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Drawing;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using System.Data;
 namespace recognTools
 {
-    public class ImageParser
+    public delegate void OnHexCodeRecognizedDelegate(string code);
+    public delegate void OnImageRecievedDelegate(Image<Bgr, Byte> sourse);
+    public delegate void OnImageFilteredDelegate(Image<Bgr, Byte> sourse);
+    public delegate void OnContourFoundDelegate(List<MCvContour> contourList);
+    public delegate void OnHexImageCroptedDelegate(Image<Bgr, Byte> sourse);
+
+    public static class ImageParser
     {
+      public static event OnHexCodeRecognizedDelegate OnHexCodeRecognized;
+      public static event OnImageRecievedDelegate OnImageRecieved;
+      public static event OnImageFilteredDelegate OnImageFiltered;
+      public static event OnContourFoundDelegate  OnContourFound;
+      public static event OnHexImageCroptedDelegate OnHexImageCropted; 
+      static void AddDevDataReciever(object reciever)
+      {
+        if (reciever is IRecieveRecognizedCode) OnHexCodeRecognized += (reciever as IRecieveRecognizedCode).Recieve;
+        if (reciever is IRecieveRawImage) OnImageRecieved += (reciever as IRecieveRawImage).Recieve;
+        if (reciever is IRecieveFilteredImage) OnImageFiltered += (reciever as IRecieveFilteredImage).Recieve;
+        if (reciever is IRecieveFoundContours) OnContourFound += (reciever as IRecieveFoundContours).Recieve;
+        if (reciever is IRecieveCroptedImage) OnHexImageCropted += (reciever as IRecieveCroptedImage).Recieve;
+
+      }
+      static void RecieveImage(Image<Bgr, Byte> sourse)
+      {
+        string result = Parse(sourse);
+        if (result != null)
+          if (OnHexCodeRecognized != null) OnHexCodeRecognized(result);
+      }
+
+      private static string Parse(Image<Bgr, Byte> sourse)
+      {
+        return null;
+      }
+
     }
 }
